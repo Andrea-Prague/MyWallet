305149aed9b2212577930e9331e3708584bceca4
Andrea Lisa <andrea.lisaa@gmail.com>
2019-11-14 18:28:14 +0100

    added BE

--- src/App.js
+++ src/App.js
@@ -3,30 +3,29 @@ import styled from "styled-components";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import DetailedOverview from "./pages/DetailedOverview";
+import { GetData } from "./components/GetData";
 
 const GlobalStyles = styled.div`
 	@import url("https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap");
 	font-family: "Roboto";
 `;
 
-const App = () => {
-	// const [neco, setNeco] = useState("")
-	// const upravState = () => {
-	// 	setNeco("neco")
-	// }
-
+const App = ({ transactionsData }) => {
 	return (
-		<GlobalStyles>
-			<BrowserRouter>
-				<Switch>
-					{/* <Route component={() => <Home upravState={upravState} />} path="/" exact /> */}
-					<Route component={Home} path="/" exact />
-					<Route component={DetailedOverview} path="/:name" />
-					{/* udelej si custom stranku na not found a pridej jako komponentu */}
-					<Route component={() => <p>404 Not Found</p>} />
-				</Switch>
-			</BrowserRouter>
-		</GlobalStyles>
+		<>
+			<GlobalStyles>
+				<BrowserRouter>
+					<Switch>
+						{/* <Route component={() => <Home upravState={upravState} />} path="/" exact /> */}
+						<Route component={Home} path="/" exact />
+						<Route component={DetailedOverview} path="/:name" />
+						{/* udelej si custom stranku na not found a pridej jako komponentu */}
+						<Route component={() => <p>404 Not Found</p>} />
+					</Switch>
+				</BrowserRouter>
+			</GlobalStyles>
+			<GetData transactionsData={transactionsData} />
+		</>
 	);
 };
 
